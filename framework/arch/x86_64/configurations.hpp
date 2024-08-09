ae471b7dc4a8d7accd1c6a05eeec19717e5affef
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-09 13:22:32 +0900

    Added architecture-specific configuration header

---
+++ framework/arch/x86_64/configurations.hpp
@@ -0,0 +1,8 @@
+// Global architecture-specific configurations for both kernel and loader.
+
+#ifndef _CONFIGURATIONS_HPP_
+#define _CONFIGURATIONS_HPP_
+
+#define KERNEL_LOCATION 0x200000
+
+#endif
