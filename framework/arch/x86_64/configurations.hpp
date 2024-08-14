2a9dcb9a3b4f8eed55e007b22a9db7bc44daa7bb
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-14 16:36:59 +0900

    Delete configurations.hpp

--- framework/arch/x86_64/configurations.hpp
+++
@@ -1,8 +0,0 @@
-// Global architecture-specific configurations for both kernel and loader.
-
-#ifndef _CONFIGURATIONS_HPP_
-#define _CONFIGURATIONS_HPP_
-
-#define KERNEL_LOCATION 0x200000
-
-#endif
