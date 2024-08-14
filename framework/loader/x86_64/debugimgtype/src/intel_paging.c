c9be02a10d059e7fd20003c7a5f46df845fb379c
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-14 16:34:23 +0900

    Fixed bugs

--- framework/loader/x86_64/debugimgtype/src/intel_paging.c
+++ framework/loader/x86_64/debugimgtype/src/intel_paging.c
@@ -9,8 +9,7 @@ void set_page_entry(page_entry_t *page_entry , unsigned int baseaddr_low , unsig
 void setup_pml4(unsigned long pml4_table_address , int pdpt_entry_count) {
     page_entry_t *pml4_entry = (page_entry_t *)pml4_table_address;
     page_entry_t *pdpt_entry = (page_entry_t *)(pml4_table_address+(PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t)));
-    page_entry_t *pde_entry =  (page_entry_t *)(pml4_table_address+(PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t))+(PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t)));
-
+    page_entry_t *pde_entry =  (page_entry_t *)((pml4_table_address+(PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t)))+(PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t)));
     /* overall structure of the PML4 entry : 
     * 
     * pml4_table_address
@@ -33,9 +32,7 @@ void setup_pml4(unsigned long pml4_table_address , int pdpt_entry_count) {
         set_page_entry(&(pml4_entry[i]) , 0x00 , 0x00 , 0x00);
     }
     set_page_entry(pml4_entry , pdpt_entry , 0 , PAGE_PML4ENTRY_FLAGS_P|PAGE_PML4ENTRY_FLAGS_RW);
-    for(unsigned int i = 0; i < PAGE_MAX_ENTRY_COUNT; i++) {
-        set_page_entry(&(pdpt_entry[i]) , 0x00 , 0x00 , 0x00);
-    }
+    
     for(unsigned int i_pdpt = 0; i_pdpt < pdpt_entry_count; i_pdpt++) {
         set_page_entry(&(pdpt_entry[i_pdpt]) , pde_entry , 0 , PAGE_PDPTENTRY_FLAGS_P|PAGE_PDPTENTRY_FLAGS_RW);
         for(unsigned int i_pde = 0; i_pde < PAGE_MAX_ENTRY_COUNT; i_pde++) {
@@ -54,6 +51,6 @@ void setup_pml4(unsigned long pml4_table_address , int pdpt_entry_count) {
             unsigned int base_high = (i_pdpt >> 2);
             set_page_entry(&(pde_entry[i_pde]) , base_low , base_high , PAGE_PDENTRY_FLAGS_P|PAGE_PDENTRY_FLAGS_RW|PAGE_PDENTRY_FLAGS_PS);
         }
-        pde_entry += (PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t));
+        pde_entry += PAGE_MAX_ENTRY_COUNT*sizeof(page_entry_t);
     }
 }
