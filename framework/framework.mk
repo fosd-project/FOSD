cf1ef72f476acf38cc1a0f6cc95a977d2936c754
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-14 16:37:32 +0900

    Update framework.mk

--- framework/framework.mk
+++ framework/framework.mk
@@ -1,35 +1,39 @@
 FRAMEWORKFOLDER = framework
-USERSOURCEFOLDER = ?
 
 include $(FRAMEWORKFOLDER)/global_variables.mk
 include $(FRAMEWORKFOLDER)/configurations.mk
 
 BINARYFOLDER = $(FRAMEWORKFOLDER)/$(ROOTBINARYFOLDER)/$(USERBINARYFOLDER)
 
-INCLUDEPATHS = . $(ARCHFOLDER)/$(ARCH)/$(COMMON_HEADERFOLDER) $(KERNELFOLDER)/$(COMMON_HEADERFOLDER)
+INCLUDEPATHS = . $(FRAMEWORKFOLDER)/$(ARCHFOLDER)/$(ARCH)/$(COMMON_HEADERFOLDER) \
+			   $(FRAMEWORKFOLDER)/$(KERNELFOLDER)/$(COMMON_HEADERFOLDER) \
+			   $(FRAMEWORKFOLDER)/$(ARCH_CONFIGURATION_FILE_LOC) \
+			   $(USER_INCLUDEPATH) \
 
 # Compile targets for user sources
-MAINTARGETS = $(subst .cpp,.obj,$(wildcard *.cpp) $(wildcard */*.cpp))
-ASMTARGETS = $(subst .asm,.aobj,$(wildcard *.asm))
+MAINTARGETS = $(subst .cpp,.obj,$(wildcard $(USER_SOURCESPATH)/*.cpp) $(wildcard $(USER_SOURCESPATH)/*/*.cpp))
+ASMTARGETS = $(subst .asm,.aobj,$(wildcard $(USER_SOURCESPATH)/*.asm))
 
 clean:
 	rm -rf $(BINARYFOLDER)
 	make -C $(FRAMEWORKFOLDER) clean
 
-all: prepare $(ASMTARGETS) $(MAINTARGETS) complete_build
+all: prepare build_user build_os
 
 prepare:
 	mkdir -p $(BINARYFOLDER)
 	make -C $(FRAMEWORKFOLDER) build_framework
 
-complete_build: # finally build kernel and loader
+build_user: $(ASMTARGETS) $(MAINTARGETS) 
+
+build_os: # finally build kernel and loader
 	make -C $(FRAMEWORKFOLDER) build_kernel build_loader
 
 %.obj: %.cpp
-	$(KERNEL_CC) $(KERNEL_CCOPTIONS) -c $< -o $(addprefix $(BINARYFOLDER)/,$@) $(KERNEL_CCOPTIONS)\
-		$(addprefix  -I ,$(INCLUDEPATHS))
+	$(KERNEL_CC) $(KERNEL_CCOPTIONS) -c $< -o $(subst $(USER_SOURCESPATH),$(BINARYFOLDER),$@) $(KERNEL_CCOPTIONS)\
+		$(addprefix  -I,$(INCLUDEPATHS))
 
 %.aobj: %.asm
-	$(KERNEL_AS) $(COMMON_SOURCEFOLDER)/$@ -o $(BINARYFOLDER)/$(subst .asm,.obj,$@)
+	$(KERNEL_AS) $< -o $(subst $(USER_SOURCESFOLDER),$(BINARYFOLDER),$@) $(KERNEL_ASOPTIONS)
 
 .PHONY: clean all
