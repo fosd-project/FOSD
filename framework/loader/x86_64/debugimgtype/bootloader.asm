ad433f146ddd1272c322be211d6561f86cfdfe71
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-14 16:31:58 +0900

    Fixed major bugs
    Fixed

--- framework/loader/x86_64/debugimgtype/bootloader.asm
+++ framework/loader/x86_64/debugimgtype/bootloader.asm
@@ -3,24 +3,34 @@
 ;;  Load the 32-bit Kernel Loader to designated location  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+[ORG 0x7C00]
 [BITS 16]
 
-    jmp 0x07C0:start
+    jmp start
 
 boot_drive:              db 0x00
 kernel_loader_location:  dw 0x1000
-kernel_loader_size:      dd 4
-kernel_location:         dw 0x2000
-kernel_size:             dd 4
+kernel_loader_size:      dd 8
+kernel_location:         dw 0x00
+kernel_location_seg:     dw 0x1000
+kernel_size:             dw 48
+
+dap_address:   dw 0x700
 
 start:
-    mov ax , 0x07C0
+    mov ax , 0x00
     mov ds , ax
 
     xor ax , ax
     mov ss , ax
-    mov sp , 0x9FF8
-    mov bp , 0x9FF8
+    mov sp , 0xFFF8
+    mov bp , 0xFFF8
+
+    ; Enable A20 gate
+	mov eax , 0x2401
+	int 0x15
+
+    xor ecx , ecx
 
     mov [boot_drive] , dl
 
@@ -30,7 +40,7 @@ start:
     mov bx , [kernel_loader_location]
 
     mov ah , 0x02
-    mov al , [kernel_loader_size]
+    mov al , byte[kernel_loader_size]
     mov ch , 0x00
     mov dh , 0x00
     mov cl , 2 ; sector location of kernel loader
@@ -40,28 +50,30 @@ start:
 
     jc failed
 
-    xor cx , cx
-    mov es , cx
-    mov bx , [kernel_location]
-
-    mov ah , 0x02
-    mov al , [kernel_size]
-    mov ch , 0x00
-    mov dh , 0x00
-    mov cl , 0x02
-    add cl , [kernel_loader_size]
-
+    xor ch , ch
+    add cx , word[kernel_loader_size]
+    dec cx
+    
+    ; use LBA
+    mov ah , 0x42
     mov dl , [boot_drive]
+    mov si , [dap_address]
+    mov byte[es:si]     , 0x10 ; DAP structure size
+    mov byte[es:si+1]   , 0x00
+    mov di , [kernel_size]
+    mov word[es:si+2]   , di
+    mov di , word[kernel_location]
+    mov word[es:si+4]   , di
+    mov di , word[kernel_location_seg]
+    mov word[es:si+6]   , di
+    mov dword[es:si+8]  , ecx     ; sector location
+    mov dword[es:si+12] , 0x00    ; (empty qword field)
     
     int 0x13
 
     jc failed
     
     cli
-
-    ; Enable A20 gate
-	mov eax , 0x2401
-	int 0x15
 
     ; switch to protect mode
 
@@ -73,6 +85,7 @@ start:
 
     ; store the kernel's location and size to the registers
     mov di , word[kernel_location]
+    mov dx , word[kernel_location_seg]
     mov si , word[kernel_size]
     
     jmp 0x08:0x1000
@@ -122,7 +135,7 @@ pmode_gdt_end:
 
 pmode_gdtr:
     .size: dw pmode_gdt_end-pmode_gdt
-    .offset: dd pmode_gdt+0x7C00
+    .offset: dd pmode_gdt
 
 times (510-($-$$)) db 0x00
 dw 0xAA55
