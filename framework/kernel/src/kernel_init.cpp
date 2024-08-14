dbbfdf8b181861e4a20348c8ee0179cff9abd356
Ian Juha Cho <ianisnumber2027@gmail.com>
2024-08-14 16:38:04 +0900

    Update kernel_init.cpp

--- framework/kernel/src/kernel_init.cpp
+++ framework/kernel/src/kernel_init.cpp
@@ -1,7 +1,13 @@
+#include <fosd/debug.hpp>
+
 extern void main(int argc , char **argv);
 
 extern "C" void kernel_init(void) {
-    char *argv[] = {"hello" , "world"};
+    // call the main kernel
+    debug::init();
+    debug::out::clear_screen(debug::out::debugcolor(DEBUG_NONE));
+
+    char *argv[] = {};
     main(0 , argv);
     
     while(1) {}
